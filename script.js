11c0bb1e3631fd3caa04f44cce99eb9def2c611c
StathisPlakakis <stathisplakorg1997@gmail.com>
2023-12-07 19:58:51 +0200

    Add on click event listeners to the buttons
    With this we log in the console the choice of user

--- script.js
+++ script.js
@@ -6,20 +6,12 @@ function getComputerChoice(min, max) {
     }
 
 
-function getPlayerChoice() {
-    playerSelection = prompt("Choose: 'Rock' - 1, 'Paper' - 2, 'Scissors' - 3")
-    while (playerSelection !== '1' && playerSelection !== '2' && playerSelection !== '3') {
-        playerSelection = prompt("Choose only between: '1' - '2' - '3'");
-    }
-    if (playerSelection === '1') {
-        playerSelection = 'Rock';
-    }else if (playerSelection === '2') {
-        playerSelection = 'Paper';
-    }else{
-        playerSelection = 'Scissors';
-    }
-    return playerSelection;
-}
+const buttons = document.querySelectorAll('button');
+buttons.forEach((button) => {
+    button.addEventListener('click',() => {
+        console.log(button.className);
+    })
+    })
 
 function playTheGame(playerSelection, computerSelection) {
     while (playerSelection === computerSelection) {
