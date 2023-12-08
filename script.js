a3f44cea494e42f931ee6c8dc7a03f30cd21059e
StathisPlakakis <stathisplakorg1997@gmail.com>
2023-12-08 20:40:19 +0200

    Add the game function
    Now, body has onclick listener, when the score is 5 the game ends

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
-function getComputerChoice(min, max) {
-    choices = ["Rock", "Paper", "Scissors"];
-    randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
-    computerSelection = choices[randomNumber];
-    return computerSelection;
-    }
+     function getComputerChoice(min, max) {
+        choices = ["Rock", "Paper", "Scissors"];
+        randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+        computerSelection = choices[randomNumber];
+        return computerSelection;
+        }
 
     function playTheGame(playerSelection, computerSelection) {
-
-        let div = document.querySelector('.content');
-        div.style.marginTop = '20px';
-
-
         if (playerSelection === computerSelection) {
             div.textContent ='It\'s a Tie. Round repeats!!!';
             return;
@@ -34,28 +29,74 @@ function getComputerChoice(min, max) {
             div.textContent =`You WIN! ${playerSelection} beats ${computerSelection}`;
             return 1;
         }
-    }
-    document.body.style.textAlign = 'center';
-    const buttons = document.querySelectorAll('button');
-    buttons.forEach((button) => {
-        button.addEventListener('click',() => {
-            const computer = getComputerChoice(0, 2);
-            const player = button.className;
-            const result = playTheGame(player, computer);
-            if (result === 1) {
-                playerScore += 1;
-                score.textContent = `Player : ${playerScore} vs Computer : ${computerScore}`;
-            }else if (result === 2) {
-                computerScore += 1;
-                score.textContent = `Player : ${playerScore} vs Computer : ${computerScore}`;
-            }
-        })
-    })
+    }   
 
-    const score = document.querySelector('.score');
-    score.style.backgroundColor = 'red';
-    let playerScore = 0;
-    let computerScore = 0;
-    score.style.fontSize = '24px';
-    score.style.marginBottom = '20px';
-    score.textContent = `Player : ${playerScore} vs Computer : ${computerScore}`;
+
+    function game() {
+        const container = document.querySelector('.container');
+        if (playerScore === 5) {
+            div.textContent = "Congratulations, you won!!!";
+            for(let i = 0; i < buttons.length; ++i) {
+                buttons[i].style.display = 'none';
+            }
+            const newBut = document.createElement('button');
+            newBut.textContent = 'Replay ?';
+            container.insertBefore(newBut, div);
+            newBut.addEventListener('click', () => {
+                div.textContent = '';
+                playerScore = 0;
+                computerScore = 0;
+                for(let j = 0; j < buttons.length; ++j) {
+                    buttons[j].style.display = 'inline-block';
+                };
+                score.textContent = `Player : ${playerScore} vs Computer : ${computerScore}`;
+                newBut.remove();
+            })
+        }else if (computerScore=== 5) {
+            div.textContent = "Game over, you Lost!";
+            for(let i = 0; i < buttons.length; ++i) {
+                buttons[i].style.display = 'none';
+            }
+            const newBut = document.createElement('button');
+            newBut.textContent = 'Replay ?';
+            container.insertBefore(newBut, div);
+            newBut.addEventListener('click', () => {
+                div.textContent = '';
+                playerScore = 0;
+                computerScore = 0;
+                for(let j = 0; j < buttons.length; ++j) {
+                    buttons[j].style.display = 'inline-block';
+                };
+                score.textContent = `Player : ${playerScore} vs Computer : ${computerScore}`;
+                newBut.remove();
+            })
+        }
+    }
+            document.body.style.textAlign = 'center';
+            document.body.addEventListener('click', game);
+            let div = document.querySelector('.content');
+            div.style.marginTop = '20px';
+            const buttons = document.querySelectorAll('button');
+            buttons.forEach((button) => {
+                button.addEventListener('click',() => {
+                    const computer = getComputerChoice(0, 2);
+                    const player = button.className;
+                    const result = playTheGame(player, computer);
+                    if (result === 1) {
+                        playerScore += 1;
+                        score.textContent = `Player : ${playerScore} vs Computer : ${computerScore}`;
+                    }else if (result === 2) {
+                        computerScore += 1;
+                        score.textContent = `Player : ${playerScore} vs Computer : ${computerScore}`;
+                    }
+                })
+            })
+        
+        
+            const score = document.querySelector('.score');
+            score.style.backgroundColor = 'red';
+            let playerScore = 0;
+            let computerScore = 0;       
+            score.style.fontSize = '24px';
+            score.style.marginBottom = '20px';
+            score.textContent = `Player : ${playerScore} vs Computer : ${computerScore}`;
